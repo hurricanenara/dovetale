a8642bd7ae27eb92497035dea1e2a2d7d0d067c9
naracodes <naralee0124@gmail.com>
2021-04-06 17:23:18 -0400

    remove unused methods in backend

--- backend/app/controllers/saved_lists_controller.rb
+++ backend/app/controllers/saved_lists_controller.rb
@@ -1,12 +1,6 @@
 class SavedListsController < ApplicationController
-    def index
-        render json: { message: "saved-list index", status: 200 }
-    end
-
     def create
-        # token = params[:access_token]|| request.headers['X-Authorization']
         gif_id = params[:gif_id]
-        # id = JWT.decode(token, Rails.application.credentials.key, true, { algorithm: 'HS256' })[0]['user_id']
         @current_user = current_user
         @record = SavedList.new(user_id: @current_user.id, gif_id: gif_id)
         if !SavedList.exists?(user_id: @current_user.id, gif_id: gif_id)
